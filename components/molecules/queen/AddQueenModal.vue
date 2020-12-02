59952cba6874fdeac59d27329c0d4eb1c74339eb
Oliver Müske <info@oliver-mueske.de>
2020-12-02 19:08:10 +0100

    Hive can be selected in modal

--- components/molecules/queen/AddQueenModal.vue
+++ components/molecules/queen/AddQueenModal.vue
@@ -21,10 +21,26 @@
           >
             <!-- This slot appears above the options from 'options' prop -->
             <template v-slot:first>
-              <b-form-select-option :value="null" disabled
+              <b-form-select-option :value="null" disabled selected
                 >-- Please select an option --</b-form-select-option
               >
             </template>
+          </b-form-select>
+          <label for="hive">Volk</label>
+          <b-form-select v-model="queen['hive']" id="hive" class="mb-3">
+            <b-form-select-option-group
+              v-for="(locHive, key) in locHives"
+              :label="key"
+              :key="key"
+            >
+              <b-form-select-option
+                v-for="hive of locHive"
+                :key="hive.value"
+                :value="hive.value"
+              >
+                {{ hive.text }}</b-form-select-option
+              >
+            </b-form-select-option-group>
           </b-form-select>
           {{ queen }}
         </b-card>
@@ -34,7 +50,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState, mapGetters } from 'vuex'
 import QueenInputField from '~/components/molecules/queen/QueenInputField.vue'
 
 export default {
@@ -62,12 +78,31 @@ export default {
         timeRange.push(i)
       }
       return timeRange
-    }
+    },
+    locHives() {
+      if (this.beeHiveList.length > 0) {
+        let location = {}
+        for (const hive of this.beeHiveList) {
+          const loc = this.getLocationById(hive.locationId).name
+          if (!location[loc]) {
+            location[loc] = []
+          }
+
+          location[loc].push({ value: hive._id, text: hive.name })
+        }
+        return location
+      }
+    },
+    ...mapState({
+      beeHiveList: (state) => state.beeHives.beeHiveList
+    }),
+    ...mapGetters('locations', ['getLocationById'])
   },
   methods: {
     async handleOk() {
       await this.addQueen(this.queen)
       this.resetModal()
+      this.setBeeHiveObject()
     },
     resetModal() {
       this.queen = {}
